e6a112ac03ebbc66c0f3f51ffee182639b249b8e
Viki Flendiansyah <vfsyah@gmail.com>
2024-11-01 21:13:08 +0700

    feat/splashscreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,31 @@
-import { View, Text, TouchableOpacity, Button } from "react-native";
-import React from "react";
-import { Href, Link, usePathname, useRouter } from "expo-router";
+import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { useRouter } from "expo-router";
+import LottieView from "lottie-react-native";
+import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-red-100">
-      <Text className="text-red-500">
-        Open up App.js to start working on your app!
-      </Text>
-      <Text className="text-4xl font-bold">Hello World! 👋</Text>
+  const router = useRouter();
 
-      {/* <TouchableOpacity onPress={() => router.replace("/home" as Href)}>
-        <Text>Home</Text>
-      </TouchableOpacity> */}
-      <Link href={"/home"}>Home</Link>
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync();
+
+    setTimeout(() => {
+      SplashScreen.hideAsync();
+      router.replace("/home");
+    }, 10000);
+  }, []);
+
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <LottieView
+        source={require("../assets/animation/Animation - 1730469803244.json")}
+        autoPlay
+        loop
+        style={{ width: 300, height: 300 }}
+      />
+      <StatusBar style="dark" />
     </View>
   );
 }
